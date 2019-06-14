591e4c65d7abc1bb3c5e31ffbb6dc2b295046bd1
Li Li <li@essential.com>
2019-06-14 01:23:14 -0700

    simple websocket server/client
    Signed-off-by: Li Li <li@essential.com>

---
+++ server.py
@@ -0,0 +1,19 @@
+from tornado import websocket
+import tornado.ioloop
+
+class EchoWebSocket(websocket.WebSocketHandler):
+    def open(self):
+        print("Websocket Opened")
+
+    def on_message(self, message):
+        print(message)
+        self.write_message(u"You said: %s" % message)
+
+    def on_close(self):
+        print("Websocket closed")
+
+application = tornado.web.Application([(r"/", EchoWebSocket),])
+
+if __name__ == "__main__":
+    application.listen(8001)
+    tornado.ioloop.IOLoop.instance().start()
